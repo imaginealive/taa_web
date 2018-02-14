247544cb0e880cffa0ebb06f7bd067c2b33719b9
Anawat <anawat@perfenterprise.com>
2018-02-14 21:40:57 +0700

    Add Membership pages
    Signed-off-by: Anawat <anawat@perfenterprise.com>

---
+++ taaproject/taaproject/Models/MembershipViewModels/MemberViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace taaproject.Models.AccountViewModels
+{
+    public class MemberViewModel
+    {
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 0)]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Display(Name = "Level")]
+        public string Level { get; set; }
+    }
+}
