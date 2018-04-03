151c37d0e1bbca796a940888b106c14f8d39b3f8
Earn <earndarkness@perfenterprise.com>
2018-04-04 00:49:55 +0700

    Implement UI
    Signed-off-by: Earn <earndarkness@perfenterprise.com>

---
+++ project_v2/project_v2/project_v2/Controllers/ProjectController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace project_v2.Controllers
+{
+    public class ProjectController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        
+        public IActionResult Detail()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
