59d2b20564ffa0f304fd733069a015cf63148c9a
Earn <earndarkness@perfenterprise.com>
2018-04-22 21:15:16 +0700

    Redesign project index page Implement Feature function (incomplete)
    Signed-off-by: Earn <earndarkness@perfenterprise.com>

---
+++ project_v2/project_v2/project_v2/Controllers/WorkController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using project_v2.Models;
+using project_v2.Services.Interface;
+
+namespace project_v2.Controllers
+{
+    public class WorkController : Controller
+    {
+        private IProjectService projectSvc;
+        private IFeatureService featureSvc;
+        public WorkController(IProjectService projectSvc,IFeatureService featureSvc)
+        {
+            this.projectSvc = projectSvc;
+            this.featureSvc = featureSvc;
+        }
+
+        public IActionResult CreateFeature(string projectid)
+        {
+            var model = projectSvc.GetProject(projectid);
+            ViewBag.ProjectName = model.ProjectName;
+            return View(new FeatureModel { Project_id = projectid });
+        }
+
+        [HttpPost]
+        public IActionResult CreateFeature(FeatureModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                featureSvc.CreateFeature(model);
+                return RedirectToAction("Index", "Project", new { projectid = model.Project_id });
+            }
+            return View(model);
+        }
+    }
+}
--- project_v2/project_v2/project_v2/Models/WorkModel.cs
+++ project_v2/project_v2/project_v2/Models/WorkModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,14 +9,38 @@ namespace project_v2.Models
     public class WorkModel
     {
         public string _id { get; set; }
+
+        [Required(ErrorMessage = "กรุณากรอกชื่อ")]
+        [Display(Name = "ชื่อ")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "กรุณากรอกรายละเอียด")]
+        [Display(Name = "รายละเอียด")]
         public string Description { get; set; }
+
+        [Display(Name = "รายงานความคืบหน้า")]
         public string WorkReport { get; set; }
+
+        [Display(Name = "วันที่สร้าง")]
         public DateTime CreateDate { get; set; }
+
+        [Required(ErrorMessage = "กรุณาระบุคาดการณ์วันที่งานต้องเสร็จสิ้น")]
+        [Display(Name = "คาดการณ์วันที่งานต้องเสร็จสิ้น")]
+        public DateTime ClosingDate { get; set; }
+
+        [Display(Name = "สร้างโดย")]
         public string CreateByMember_id { get; set; }
+
+        [Display(Name = "มอบหมายงานโดย")]
         public string AssginByMember_id { get; set; }
+
+        [Display(Name = "ผู้รับมอบหมายหมาย")]
         public string BeAssignedMember_id { get; set; }
+
+        [Display(Name = "สถานะของงาน")]
         public string StatusName { get; set; }
+
+        [Display(Name = "วันที่งานเสร็จสิ้นจริง")]
         public DateTime? WorkDoneDate { get; set; }
     }
 }
