5c5cf3283226f479581394ca8b6c26a6cd30af3a
Earn <earndarkness@perfenterprise.com>
2018-05-22 00:06:50 +0700

    Implement Graph Index
    Signed-off-by: Earn <earndarkness@perfenterprise.com>

--- project_v2/project_v2/project_v2/Controllers/HomeController.cs
+++ project_v2/project_v2/project_v2/Controllers/HomeController.cs
@@ -21,17 +21,23 @@ namespace project_v2.Controllers
         private IStoryService storySvc;
         private ITaskService taskSvc;
         public IDistributedCache cache;
+        private IRankService rankSvc;
+        private IMembershipService membershipSvc;
         public HomeController(IProjectService projectSvc,
             IFeatureService featureSvc,
             IStoryService storySvc,
             ITaskService taskSvc,
-            IDistributedCache _cache)
+            IDistributedCache _cache,
+            IRankService rankSvc,
+            IMembershipService membershipSvc)
         {
             cache = _cache;
             this.projectSvc = projectSvc;
             this.featureSvc = featureSvc;
             this.storySvc = storySvc;
             this.taskSvc = taskSvc;
+            this.rankSvc = rankSvc;
+            this.membershipSvc = membershipSvc;
         }
 
         public IActionResult Index()
@@ -42,44 +48,66 @@ namespace project_v2.Controllers
             
             ViewBag.IsLogin = !string.IsNullOrEmpty(cache.GetString("user"));
 
-            var model = projectSvc.GetProjects(ViewBag.User._id);
+            var model = projectSvc.GetProjects(currentUser._id);
+            var ranks = rankSvc.GetAllRank();
+            var memberships = Enumerable.Empty<ProjectMembershipModel>();
+            foreach (var item in model) memberships = membershipSvc.GetAllProjectMember(item._id);
 
+            var members = currentUser != null ? memberships.Where(it => it.Account_id == currentUser._id && !it.RemoveDate.HasValue) : null;
             var GraphData = new List<ProjectGraphModel>() { };
             foreach (var project in model)
             {
-                var data = new ProjectGraphModel()
-                {
-                    ProjectId = project._id,
-                    ProjectName = project.ProjectName
-                };
+                var data = new ProjectGraphModel() { ProjectId = project._id, ProjectName = project.ProjectName };
                 List<FeatureModel> features = featureSvc.GetFeatures(project._id);
+                var member = members.FirstOrDefault(it => it.Project_id == project._id);
+                var CanSeeAllWork = member != null ? (ranks.FirstOrDefault(it => it._id == member.ProjectRank_id).CanSeeAllWork) || member.CanSeeAllWork : false;
                 var AllWork = 0;
                 var WorkDone = 0;
+
                 foreach (var feature in features)
                 {
-                    List<StoryModel> stories = storySvc.GetStories(feature._id);
+                    var ownerThisFeatureWork = false;
+                    var BeAssignedThisFeature = members.Any(it => it.Account_id == feature.BeAssignedMember_id && it.Project_id == feature.Project_id);
+
+                    ownerThisFeatureWork = BeAssignedThisFeature || CanSeeAllWork;
+
+                    var stories = storySvc.GetStories(feature._id);
                     foreach (var story in stories)
                     {
-                        List<TaskModel> tasks = taskSvc.GetTasks(story._id);
-                        AllWork = AllWork + tasks.Count();
-                        WorkDone = WorkDone + tasks.Where(it => it.WorkDoneDate.HasValue).Count();
+                        var ownerThisStoryWork = false;
+                        ownerThisStoryWork = members.Any(it => it.Account_id == story.BeAssignedMember_id) || CanSeeAllWork || ownerThisFeatureWork;
 
-                        if (story.WorkDoneDate.HasValue)
+                        var tasks = taskSvc.GetTasks(story._id);
+                        foreach (var task in tasks)
                         {
-                            WorkDone++;
+                            var ownerThisTaskWork = false;
+                            ownerThisTaskWork = members.Any(it => it.Account_id == task.BeAssignedMember_id) || CanSeeAllWork || ownerThisStoryWork;
+
+                            if (ownerThisTaskWork)
+                            {
+                                AllWork++;
+                                if (task.WorkDoneDate.HasValue) WorkDone++;
+                            }
                         }
-                        AllWork++;
+
+                        if (ownerThisStoryWork)
+                        {
+                            AllWork++;
+                            if (story.WorkDoneDate.HasValue) WorkDone++;
+                        }
                     }
-                    if (feature.WorkDoneDate.HasValue)
+                    if (ownerThisFeatureWork)
                     {
-                        WorkDone++;
+                        AllWork++;
+                        if (feature.WorkDoneDate.HasValue) WorkDone++;
                     }
-                    AllWork++;
                 }
                 data.WorkDone = WorkDone;
-                data.WorkProcess = AllWork - WorkDone;
+                data.WorkProcess = AllWork;
                 GraphData.Add(data);
             }
+
+            ViewBag.CanCreateProject = currentUser.ProjectCreatable;
             ViewBag.Data = GraphData;
             return View(model);
         }
